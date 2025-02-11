f111052b32b8a2bf746710cea7573c70f6242c5c
mytlx <mytlx1466@gmail.com>
2025-02-11 16:41:47 +0800

    Initial commit
    Signed-off-by: mytlx <mytlx1466@gmail.com>

---
+++ src/main/java/com/mytlx/Main.java
@@ -0,0 +1,12 @@
+package com.mytlx;
+
+/**
+ * @author TLX
+ * @version 1.0.0
+ * @since 2025-02-11 16:12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
