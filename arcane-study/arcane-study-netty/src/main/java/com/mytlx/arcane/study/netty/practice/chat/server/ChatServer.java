1ee4aa58fdd45fa78e0ef9f31422f05f015014bb
mytlx <mytlx1466@gmail.com>
2025-09-07 20:14:17 +0800

    feat(arcane-study-netty): netty chat add idle check
    Signed-off-by: mytlx <mytlx1466@gmail.com>

--- arcane-study/arcane-study-netty/src/main/java/com/mytlx/arcane/study/netty/practice/chat/server/ChatServer.java
+++ arcane-study/arcane-study-netty/src/main/java/com/mytlx/arcane/study/netty/practice/chat/server/ChatServer.java
@@ -5,11 +5,16 @@ import com.mytlx.arcane.study.netty.practice.chat.protocol.ProtocolFrameDecoder;
 import com.mytlx.arcane.study.netty.practice.chat.server.handler.*;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -40,6 +45,26 @@ public class ChatServer {
             bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
                 @Override
                 protected void initChannel(SocketChannel ch) throws Exception {
+
+                    // 用来判断是不是 读空闲时间过长 或 写空闲时间过长
+                    // 5s内如果没有收到 channel 的数据，会触发一个事件 IdleState#READER_IDLE
+                    ch.pipeline().addLast(new IdleStateHandler(5, 0, 0));
+                    // ChannelDuplexHandler 可以同时作为入站和出站处理器
+                    ch.pipeline().addLast(new ChannelDuplexHandler() {
+                        /**
+                         * 用来触发特殊事件
+                         */
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+                            IdleStateEvent event = (IdleStateEvent) evt;
+                            // 触发了读空闲事件
+                            if (event.state() == IdleState.READER_IDLE) {
+                                log.debug("已经5s没有读到数据了");
+                            }
+
+                        }
+                    });
+
                     ch.pipeline()
                             .addLast(new ProtocolFrameDecoder())
                             .addLast(loggingHandler)
