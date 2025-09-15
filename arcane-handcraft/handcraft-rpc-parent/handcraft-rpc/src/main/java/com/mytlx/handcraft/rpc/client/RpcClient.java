a9378d9094e5ca1714f51d7d1a979c8e5623a294
mytlx <mytlx1466@gmail.com>
2025-09-15 16:19:38 +0800

    fix(arcane-handcraft-rpc): reconnect and stop
    Signed-off-by: mytlx <mytlx1466@gmail.com>

--- arcane-handcraft/handcraft-rpc-parent/handcraft-rpc/src/main/java/com/mytlx/handcraft/rpc/client/RpcClient.java
+++ arcane-handcraft/handcraft-rpc-parent/handcraft-rpc/src/main/java/com/mytlx/handcraft/rpc/client/RpcClient.java
@@ -32,6 +32,7 @@ import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author TLX
@@ -69,10 +70,16 @@ public class RpcClient implements SmartInitializingSingleton, ApplicationContext
     @Value("${handcraft.rpc.server.port}")
     private int port;
 
+    private AtomicInteger retryCount = new AtomicInteger(0);
+
+    private int maxRetryTimes = 5;
+
+
     @PostConstruct
     public void init() {
         log.info("rpc client init");
-        new Thread(this::connect).start();
+        connect();
+        Runtime.getRuntime().addShutdownHook(new Thread(this::stop));
     }
 
     public void connect() {
@@ -108,30 +115,52 @@ public class RpcClient implements SmartInitializingSingleton, ApplicationContext
                             });
                         }
                     });
-            ChannelFuture cf = bootstrap.connect(host, port)
+            bootstrap.connect(host, port)
                     .addListener((ChannelFutureListener) future -> {
                         if (future.isSuccess()) {
                             log.info("client id: {} 连接成功", clientId);
+                            retryCount.set(0);
                             this.channel = future.channel();
                             // 连接成功，发送注册消息
                             sendRegistrationRequest();
+                            future.channel().closeFuture().addListener(
+                                    cf -> {
+                                        log.warn("Channel closed, try to reconnect...");
+                                        reconnect();
+                                    }
+                            );
                         } else {
-                            log.error("Failed to connect to server", future.cause());
+                            log.error("Failed to connect to server: {}", future.cause().getMessage(), future.cause());
                             reconnect();
                         }
                     });
-
-            cf.channel().closeFuture().sync();
         } catch (Exception e) {
             log.error("Failed to connect to rpc server", e);
             throw new RuntimeException(e);
-        } finally {
-            worker.shutdownGracefully();
         }
     }
 
-    public void reconnect() {
+    public boolean reconnect() {
+        if (retryCount.getAndIncrement() >= maxRetryTimes) {
+            log.error("retry times has been limited: {}", maxRetryTimes);
+            return false;
+        }
+        log.info("reconnect to server, times: {}", retryCount.get());
         worker.schedule(RpcClient.this::connect, 5, TimeUnit.SECONDS);
+        return true;
+    }
+
+    public void stop() {
+        log.info("正在关闭客户端 {}", clientId);
+        try {
+            if (channel != null) {
+                channel.close().sync();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            worker.shutdownGracefully();
+        }
     }
 
     public void sendRegistrationRequest() {
